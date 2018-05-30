15422bc238cab057b82b5c91b58f787f65923230
Kjell-Morten <post@kjellmorten.no>
2018-05-30 18:36:31 +0200

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Definitions file for integreat-adapter-soap
+
+export function prepareEndpoint(endpointOptions: object, serviceOptions: object): object
+export function send(request: object): Promise<object>
+export function normalize(data: object, request: object): Promise<object>
+export function serialize(data: object, request: object): Promise<object>
