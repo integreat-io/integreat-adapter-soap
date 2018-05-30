4ec487b37fc577a2bb2017a7cf1b34c3e1905726
Kjell-Morten <post@kjellmorten.no>
2018-05-30 18:56:10 +0200

    Include http headers from auth

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 // Definitions file for integreat-adapter-soap
 
+export const authentication: string
 export function prepareEndpoint(endpointOptions: object, serviceOptions: object): object
 export function send(request: object): Promise<object>
 export function normalize(data: object, request: object): Promise<object>
