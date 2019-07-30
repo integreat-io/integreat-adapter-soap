65f682bd25fef780704c8ccf420b3cb01ade917b
Kjell-Morten <post@kjellmorten.no>
2019-07-30 19:10:18 +0200

    Update typing

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
 // Definitions file for integreat-adapter-soap
 
-export const adapter: {
+interface Adapter {
   authentication: string,
   prepareEndpoint: (endpointOptions: object, serviceOptions: object) => object,
   send: (request: object) => Promise<object>,
   normalize: (data: object, request: object) => Promise<object>,
   serialize: (data: object, request: object) => Promise<object>
 }
+
+interface Logger {
+  info: (...args: any[]) => void
+  error: (...args: any[]) => void
+}
+
+declare const _default: (logger?: Logger) => Adapter
+
+export = _default
