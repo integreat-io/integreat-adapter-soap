178876fb66a7def3c4d657da554f684f182ca21a
Kjell-Morten <post@kjellmorten.no>
2022-01-19 08:27:13 +0100

    Include data from error response

--- tests/log-test.js
+++ tests/log-test.js
@@ -7,14 +7,15 @@ import soap from '..'
 
 // Setup
 
-const requestBody = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetInvoices xmlns="http://api1.test/webservices"><searchParams><CustomerIds><int>18003</int></CustomerIds><InvoiceIds><int>341101</int><int>341102</int></InvoiceIds></searchParams><invoiceReturnProperties><string>InvoiceId</string><string>CustomerId</string><string>CustomerName</string><string>OrderStatus</string></invoiceReturnProperties></GetInvoices></soap:Body></soap:Envelope>/'
+const requestBody =
+  '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetInvoices xmlns="http://api1.test/webservices"><searchParams><CustomerIds><int>18003</int></CustomerIds><InvoiceIds><int>341101</int><int>341102</int></InvoiceIds></searchParams><invoiceReturnProperties><string>InvoiceId</string><string>CustomerId</string><string>CustomerName</string><string>OrderStatus</string></invoiceReturnProperties></GetInvoices></soap:Body></soap:Envelope>/'
 
 // Tests
 
-test('should log request and response', async t => {
+test('should log request and response', async (t) => {
   const logger = {
     info: sinon.stub(),
-    error: sinon.stub()
+    error: sinon.stub(),
   }
   const adapter = soap(logger)
   nock('http://api1.test')
@@ -25,22 +26,24 @@ test('should log request and response', async t => {
     endpoint: {
       uri: 'http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx',
       path: 'GetInvoicesResponse.GetInvoicesResult.InvoiceOrder',
-      namespace: 'http://api1.test/webservices'
+      namespace: 'http://api1.test/webservices',
     },
-    data: requestBody
+    data: requestBody,
   }
-  const expectedPreMessage = 'Sending POST http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx'
+  const expectedPreMessage =
+    'Sending POST http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx'
   const expectedPreMeta = {
     method: 'POST',
     uri: 'http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx',
     body: requestBody,
     headers: { 'Content-Type': 'text/xml;charset=utf-8' },
-    retries: 0
+    retries: 0,
   }
-  const expectedPostMessage = 'Success from POST http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx'
+  const expectedPostMessage =
+    'Success from POST http://api1.test/Economy/InvoiceOrder/V001/InvoiceService.asmx'
   const expectedPostMeta = {
     status: 'ok',
-    data: soapResponseInvoices
+    data: soapResponseInvoices,
   }
 
   await adapter.send(request)
@@ -55,10 +58,10 @@ test('should log request and response', async t => {
   nock.restore()
 })
 
-test('should log error response', async t => {
+test('should log error response', async (t) => {
   const logger = {
     info: sinon.stub(),
-    error: sinon.stub()
+    error: sinon.stub(),
   }
   const adapter = soap(logger)
   nock('http://api2.test')
@@ -69,14 +72,16 @@ test('should log error response', async t => {
     endpoint: {
       uri: 'http://api2.test/Economy/InvoiceOrder/V001/InvoiceService.asmx',
       path: 'GetInvoicesResponse.GetInvoicesResult.InvoiceOrder',
-      namespace: 'http://api1.test/webservices'
+      namespace: 'http://api1.test/webservices',
     },
-    data: requestBody
+    data: requestBody,
   }
-  const expectedPostMessage = 'Error from POST http://api2.test/Economy/InvoiceOrder/V001/InvoiceService.asmx: notfound'
+  const expectedPostMessage =
+    'Error from POST http://api2.test/Economy/InvoiceOrder/V001/InvoiceService.asmx: notfound'
   const expectedPostMeta = {
     status: 'notfound',
-    error: 'Could not find \'http://api2.test/Economy/InvoiceOrder/V001/InvoiceService.asmx\': HTTPError: Response code 404 (Not Found)'
+    error:
+      "Could not find 'http://api2.test/Economy/InvoiceOrder/V001/InvoiceService.asmx': Not Found",
   }
 
   await adapter.send(request)
